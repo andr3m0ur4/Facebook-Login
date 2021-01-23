9a7d8a9d00a3fef5ba7fd2158b438ab9cf2638d0
André Moura <andre.benedicto@etec.sp.gov.br>
2021-01-23 18:55:10 -0300

    obtendo mais alguns dados

--- index.php
+++ index.php
@@ -6,13 +6,23 @@
         header('Location: login.php');
     }
 
-    $response = $fb->get('/me?fields=email,name,id', $_SESSION['fb_access_token']);
+    $response = $fb->get('/me?fields=email,name,id,picture', $_SESSION['fb_access_token']);
 
-    $result = json_decode($response->getBody());
+    $result = $response->getGraphNode();
+
+    $img = $result->getField('picture')->getField('url');
+    $user_id = $result->getField('id');
+
+    $response = $fb->get(
+        $user_id . '?fields=birthday,email,hometown',
+        $_SESSION['fb_access_token']
+    );
+    $result = $response->getGraphNode();
 
     echo '<pre>';
     print_r($result);
 
 ?>
 <br>
+<img src="<?= $img ?>" alt=""> <br>
 <a href="sair.php">Sair</a>
